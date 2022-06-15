cb9be4db3456e379fbad3ba86d6677754ca7df63
Harshit Kumar <bkharshitbk@gmail.com>
2022-06-15 21:24:25 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HEY</h1>
-    </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
